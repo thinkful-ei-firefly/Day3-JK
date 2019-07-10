4a55207a8ac3188f502fa6897a3edf98ab695178
Keith Prince <keithcprince@gmail.com>
2019-07-10 11:34:54 -0400

    Enroll in summer school

--- page2.js
+++ page2.js
@@ -17,3 +17,28 @@ const testData = [
   { name: 'Jill Do', grade: 'A' },
 ];
 
+function enrollInSummerSchool(students){
+    for (let i = 0; i < students.length; i++){
+        students[i].status = "In Summer School";
+    }
+    return students;
+}
+
+const studentData = [
+    {
+      name: 'Tim',
+      status: 'Current student',
+      course: 'Biology',
+    },
+    {
+      name: 'Sue',
+      status: 'Withdrawn',
+      course: 'Mathematics',
+    },
+    {
+      name: 'Liz',
+      status: 'On leave',
+      course: 'Computer science',
+    },
+  ];
+
