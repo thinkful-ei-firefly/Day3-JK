b611a258cdb1e93fdcf779d65ac45ba8bc50ba91
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-10 11:42:46 -0400

    IDFind

--- page2.js
+++ page2.js
@@ -18,27 +18,39 @@ const testData = [
 ];
 
 function enrollInSummerSchool(students){
-    for (let i = 0; i < students.length; i++){
-        students[i].status = "In Summer School";
-    }
-    return students;
+  for (let i = 0; i < students.length; i++){
+    students[i].status = "In Summer School";
+  }
+  return students;
 }
 
 const studentData = [
-    {
-      name: 'Tim',
-      status: 'Current student',
-      course: 'Biology',
-    },
-    {
-      name: 'Sue',
-      status: 'Withdrawn',
-      course: 'Mathematics',
-    },
-    {
-      name: 'Liz',
-      status: 'On leave',
-      course: 'Computer science',
-    },
-  ];
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
 
+
+function findById (items, idNum) {
+  for (let i=0; i <items.length; i++) {
+    if (items[i].id === idNum) {
+      return items[i];
+    }
+  }
+}
+
+
+const data = [{id: 1, foo: 'bar'}, {id: 2, foo: 'bizz'}];
+//console.log(findById(data, 2)); // should return {id: 2, foo: 'bizz'}
