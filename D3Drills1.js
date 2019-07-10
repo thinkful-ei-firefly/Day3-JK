345bbc9e5c50c440e6c2efec0b5998d10d18be32
Jess Doyle <jessica.e.doyle@gmail.com>
2019-07-10 11:17:00 -0400

    P3

--- D3Drills1.js
+++ D3Drills1.js
@@ -23,4 +23,26 @@ function updateObject(obj){
 let newObj = updateObject(thisObj);
 console.log(newObj.bizz);
 
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+  };
+  return person;
+}
 
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  // your code here
+}
